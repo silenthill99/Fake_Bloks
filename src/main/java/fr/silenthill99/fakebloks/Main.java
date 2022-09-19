76add0af196e83433f5fc2506fbe8fc598196438
silenthill99 <florian.graziani@sfr.fr>
2022-09-19 13:21:55 +0200

    Initialisation du projet

---
+++ src/main/java/fr/silenthill99/fakebloks/Main.java
@@ -0,0 +1,31 @@
+package fr.silenthill99.fakebloks;
+
+import fr.silenthill99.fakebloks.init.ModItems;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Main.MODID)
+public class Main
+{
+    public static final String MODID = "fake_bloks";
+
+    public Main()
+    {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::setup);
+        bus.addListener(this::clientSetup);
+        ModItems.ITEMS.register(bus);
+    }
+
+    private void setup(FMLCommonSetupEvent event)
+    {
+    }
+
+    private void clientSetup(FMLClientSetupEvent event)
+    {
+    }
+}
---
+++ src/main/java/fr/silenthill99/fakebloks/init/ModItems.java
@@ -0,0 +1,11 @@
+package fr.silenthill99.fakebloks.init;
+
+import fr.silenthill99.fakebloks.Main;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.item.Item;
+
+public class ModItems
+{
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MODID);
+}
